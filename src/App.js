2d45a5afb482778cc45797eae1ee8f8c7a20937e
isaac <si.hyeon.chan@gmail.com>
2020-09-29 14:39:54 +0900

    영화앱 기본구조

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  componentDidMount() {
+    // 영화 로딩
+    setTimeout(() => this.setState({ isLoading: false }), 6000);
+  }
+
+  render() {
+    const { isLoading } = this.state
+    return (
+      <div>
+        {isLoading ? "Loading..." : "We are ready"}
+      </div>
+    )
+  }
 }
-
-export default App;
