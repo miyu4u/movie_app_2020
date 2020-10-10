436f6b58e5e2f39116d615f1229027de956c49c9
isaac <si.hyeon.chan@gmail.com>
2020-10-11 00:58:02 +0900

    Hashrouter 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import './app.css'
+import { HashRouter, Route } from 'react-router-dom'
+import { About } from './routes/about'
 import { Home } from './routes/home'
-
 
 export class App extends Component {
 
 
     render() {
-        return <Home />
+        return (
+            <HashRouter>
+                <Route path="/" exact={true} component={Home} />
+                <Route path="/about" component={About} />
+            </HashRouter>
+        )
     }
 }
