aeea189cfa15fa286396c4b9605989d901a18af4
isaac <si.hyeon.chan@gmail.com>
2020-10-11 00:25:25 +0900

    장르 추가

--- src/App.js
+++ src/App.js
@@ -5,46 +5,47 @@ import './app.css'
 const endpoint = 'https://yts.mx/api/v2/list_movies.json?sort_by=rating'
 
 export class App extends Component {
-  state = {
-    isLoading: true,
-    movies: []
-  }
+    state = {
+        isLoading: true,
+        movies: []
+    }
 
-  async getMovies() {
-    const { data: { data: { movies } } } = await axios.get(endpoint)
-    this.setState({
-      movies,
-      isLoading: false
-    })
-    console.log(this.state.movies)
-  }
+    async getMovies() {
+        const { data: { data: { movies } } } = await axios.get(endpoint)
+        this.setState({
+            movies,
+            isLoading: false
+        })
+        console.log(this.state.movies)
+    }
 
-  componentDidMount() {
-    this.getMovies()
-  }
+    componentDidMount() {
+        this.getMovies()
+    }
 
-  render() {
-    const { isLoading, movies } = this.state
-    return (
-      <section className="container">
-        {
-          isLoading ?
-            (<div className="loader">
-              <span className="loader__text">Loading</span>
-            </div>)
-            : (
-              <div className="movies">
-                {movies.map(movie => (
-                  <Movie
-                    key={movie.id}
-                    year={movie.year}
-                    title={movie.title}
-                    summary={movie.summary}
-                    poster={movie.medium_cover_image}
-                  />))}
-              </div>
-            )}
-      </section>
-    )
-  }
+    render() {
+        const { isLoading, movies } = this.state
+        return (
+            <section className="container">
+                {
+                    isLoading ?
+                        (<div className="loader">
+                            <span className="loader__text">Loading</span>
+                        </div>)
+                        : (
+                            <div className="movies">
+                                {movies.map(movie => (
+                                    <Movie
+                                        key={movie.id}
+                                        year={movie.year}
+                                        title={movie.title}
+                                        summary={movie.summary}
+                                        poster={movie.medium_cover_image}
+                                        genres={movie.genres}
+                                    />))}
+                            </div>
+                        )}
+            </section>
+        )
+    }
 }
--- src/movie.js
+++ src/movie.js
@@ -2,13 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './movie.css'
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
     return (
         <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie_data">
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {
+                        genres.map((genre, index) => <li key={index} className="movie_genre">{genre}</li>)
+                    }
+                </ul>
                 <p className="movie_summary">{summary}</p>
             </div>
         </div>
@@ -20,7 +25,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 
